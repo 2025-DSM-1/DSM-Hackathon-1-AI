a0e631b4693d3ebc83dd7c340af9c90c36c415d2
Choyoungjun <cyj070513@gmail.com>
2025-07-15 08:44:40 +0900

    수정

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:/opt/render/.local/bin
 
 # 패키지 설치
 pip3 install --upgrade pip
-pip3 install --user -r requirements.txt
+pip3 install -r requirements.txt
 
 # 서버 실행
 uvicorn main:app --host 0.0.0.0 --port 8000
